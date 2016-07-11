524caa80604fdaabf4e3867a975f7d871daa62be
HaritaReddy <harita_1997@yahoo.com>
2016-07-12 00:05:28 +0530

    Delete refine.sh

--- refine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/
-echo "Enter the name with which you want to save the book"
-read book
-sed -e 's:<p.*>: :g; s:</p>: :g; s:<br />: :g; s:<br/>: :g; s:<em.*>: :g; s:</em>: :g; s:<strong.*>: :g; s:</strong>: :g' inter | cat>"$book"
-rm inter
-
