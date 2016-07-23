82d3aff73bc3bca6c100f983792c0f5ec7f97f18
HaritaReddy <harita_1997@yahoo.com>
2016-07-23 19:28:10 +0530

    Update refine.sh

--- refine.sh
+++ refine.sh
@@ -1,6 +1,7 @@
 #!/bin/bash/
 echo "Enter the name with which you want to save the book"
 read book
+#Removes html tags
 sed -e 's:<p.*>: :g; s:</p>: :g; s:<br />: :g; s:<br/>: :g; s:<em.*>: :g; s:</em>: :g; s:<strong.*>: :g; s:</strong>: :g' inter | cat>"$book"
 rm inter
 
