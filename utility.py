f252cda5f86e6bbdc4e254890d33199702e277fe
HaritaReddy <harita_1997@yahoo.com>
2016-07-23 19:27:20 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,22 +1,15 @@
 import bs4
 import re
+#Gives month number based on the name
 def givemonth(obj):
  months={"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
  return months[obj]
-
+ 
+#For printing train status table
 def gettable(text):
  mytable=text.select('table')
  rowlist=mytable[0].select('tr')
  headers=(rowlist[0]).select("th")
-# print "\n\n"
-# for i in range(0,num):
-#  print headers[i].getText(),"\t",
-# print "\n" 
-# for i in range(1,len(rowlist)):
-#  rowitems=rowlist[i].select("td")
-#  for j in range(0,len(rowitems)):
-#  print rowitems[j].getText(),"\t",
-#  print "\n"
  for i in range(0,5):
   print "%25s"%(headers[i].getText()),
  print "\n"
